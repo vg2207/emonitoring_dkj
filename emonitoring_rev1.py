6d961eb52ca8b11820292a9b0855a9703dd9bb4d
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-10-02 16:32:01 +0700

    Update emonitoring_rev1.py

--- emonitoring_rev1.py
+++ emonitoring_rev1.py
@@ -204,8 +204,8 @@ if st.session_state["page"] == 1.5:
         key="selected_feeders"
     )
 
-    left_1.5, middle_1.5, right_1.5 = st.columns([1,3,1])
-    with right_1.5:
+    left_1_5, middle_1_5, right_1_5 = st.columns([1,3,1])
+    with right_1_5:
         if st.button("Next Page ►"):
             if not selected_feeders:
                 st.warning("Pilih minimal 1 feeder sebelum lanjut!")
@@ -213,7 +213,7 @@ if st.session_state["page"] == 1.5:
                 st.session_state.page = 2
                 st.rerun()
     
-    with left_1.5:
+    with left_1_5:
         if st.button("◄ Back"):
             st.session_state.page = 1
             st.rerun()
