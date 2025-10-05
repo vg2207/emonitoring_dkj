cd8dedf5b0b937ce3816db2a272a424e6d2db80d
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-10-05 18:27:54 +0700

    Update emonitoring_rev1.py

--- emonitoring_rev1.py
+++ emonitoring_rev1.py
@@ -287,7 +287,7 @@ if st.session_state["page"] == 2:
         # liquid_feeder_option = None
 
 
-        if df_data_page_1_5["Feeder yang digunakan"].isin(["Feeder 1 - F1"]):
+        if df_data_page_1_5["Feeder yang digunakan"].isin("Feeder 1 - F1"):
             with st.container(border=True):
                 st.markdown(f"<h5 style='text-align: left;'>Output Feeder 1 - F1 (kg/jam)</h5>", unsafe_allow_html=True)
                 col_2_1, col_2_2 = st.columns([1, 1])
