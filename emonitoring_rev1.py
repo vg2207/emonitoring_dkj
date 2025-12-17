a9d32c8be1d7a7b03e20a42bdfcf78aaddd08a17
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-12-17 10:21:15 +0700

    Update emonitoring_rev1.py

--- emonitoring_rev1.py
+++ emonitoring_rev1.py
@@ -2673,18 +2673,39 @@ if "initialized" not in st.session_state:
 
 
 
-# col_0_1, col_0_2, col_0_3 = st.columns([1, 3, 1])
-# with col_0_1 :
-#     logo = Image.open('assets_logo/Logo Dunia Kimia Jaya.png')
-# with col_0_2 :
+
 logo = Image.open('assets_logo/Logo Dunia Kimia Jaya.png')
-# logo = Image.open("D:\Coding\Project Form Monitoring\Logo\logo.jpg")
-# col_00, col_01, col_02 = st.columns([1, 3, 1])
-# with col_01:
-#     st.image(logo)
 
 st.logo(logo, size="large")
 
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 
 # Halaman Awal
 
